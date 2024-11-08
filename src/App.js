f3b196623746a8440d3ee83a35a1ecc70ea20197
Mathi <124028262+Mathiarasu05@users.noreply.github.com>
2024-11-08 14:20:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world - Mathiarasu R S - 22CB033
+          Hello world! - Mathiarasu R S - 22CB033
         </p>
       </header>
     </div>
